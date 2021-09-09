358f34c27ddb25cd02ff073faf0a6670996a901e
Alexandre Santana <alexandre.rafael07@gmail.com>
2021-09-09 13:40:27 -0300

    Exercício sem sucesso

---
+++ semana5/meu-perfil-profissional/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem=""
+          nome="Alexandre R. N. Santana" 
+          descricao="Olá, meu nome é Alexandre, moro em Curitiba/PR, sou casado, pai de 3 filhos e estou me qualificando para ser um Desenvolvedor Web Full Stack."
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem=""
+          nome="Raffah Ltda." 
+          descricao="Empresa de representação industrial no segmento de embalagens descartáveis." 
+        />
+        
+        <CardGrande 
+          imagem="" 
+          nome="" 
+          descricao="" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
